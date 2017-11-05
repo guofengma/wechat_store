fdd078d205bf47f134608c8af4e13c4f3c865fd5
ChainXRF <15801234157@126.com>
2017-11-05 16:25:23 +0800

    增加地址

--- quick/pages/apply/apply.js
+++ quick/pages/apply/apply.js
@@ -1,9 +1,9 @@
 // pages/apply/apply.js
 import fetch from '../../utils/fetch';
 
-let storeList = [];
-let page = 0;
-let totalpage = 0;
+let storeList = []
+let page = 0
+let totalpage = 0
 Page({
 
   /**
@@ -55,71 +55,19 @@ Page({
       url: '../shelvenav/shelvenav',
     })
   },
-  searchstore(){
+  setItemStorename(e) {
+    
+    var idx = e.target.dataset.idx
+    // this.data.storeList[idx].storename = e.detail.value
 
-    storeList = []
-    this.data.storeList = []
-    page = 0
-    this.querystore()
+    // 更新地址
+
+
   },
-  querystore() {
-
-    console.log('address' + this.data.address)
-
-    this.data.hasOrder = false
-
-    fetch({
-      url: "/CVS/apply/queryapply",
-      //   baseUrl: "http://192.168.50.57:9888",
-      baseUrl: "https://store.lianlianchains.com",
-      data: {
-        'page': page,
-        'pagenum': 5,
-        'address': this.data.address
-      },
-      method: "GET",
-      noLoading: true,
-      header: { 'content-type': 'application/x-www-form-urlencoded' }
-    }).then(res => {
-
-      console.log(res.apply)
-
-      totalpage = res.totalpage
-      this.setData({
-        storeList: this.data.storeList.concat(res.apply)
-      })
-      this.data.hasOrder = true
-
-    }).catch(err => {
-
-      wx.showToast({
-        title: '出错了',
-      })
-      console.log(err)
-    })
-  },
-  setAddress(e){
+  setAddress(e) {
     this.setData({
       address: e.detail.value
     })
-  },
-  loadMore() {
-
-    if (page >= totalpage/5) {
-
-      console.log("没有更多了")
-      page = totalpage
-
-      this.setData({
-        hidden: false
-      })
-    } else {
-
-      console.log("加载更多")
-      page++
-
-      this.querystore(this.data.address)
-    }
   },
   checkrole(roletype) {
 
@@ -154,47 +102,94 @@ Page({
 
     }
   },
+  searchstore() {
+
+    storeList = []
+    this.data.storeList = []
+    page = 0
+    this.querystore()
+  },
+  querystore() {
+
+    this.data.hasOrder = false
+
+    fetch({
+      url: "/CVS/apply/queryapply",
+      //   baseUrl: "http://192.168.50.57:9888",
+      baseUrl: "https://store.lianlianchains.com",
+      data: {
+        'page': page,
+        'pagenum': 5,
+        'address': this.data.address
+      },
+      method: "GET",
+      noLoading: true,
+      header: { 'content-type': 'application/x-www-form-urlencoded' }
+    }).then(res => {
+
+      // console.log(res.apply)
+
+      totalpage = res.totalpage
+      this.setData({
+        storeList: this.data.storeList.concat(res.apply)
+      })
+      this.data.hasOrder = true
+
+    }).catch(err => {
+
+      wx.showToast({
+        title: '出错了',
+      })
+      console.log(err)
+    })
+  },
+  loadMore() {
+
+    // console.log(totalpage)
+    // console.log(page)
+
+    if (page >= totalpage / 5 + 1) {
+
+      console.log("没有更多了")
+      page = totalpage
+
+      this.setData({
+        hidden: false
+      })
+    } else {
+
+      console.log("加载更多")
+      page++
+
+      this.querystore(this.data.address)
+    }
+  },
   inittapicon(e) {
 
     // 0 场地 1 经营 2 供货
     var roletype = parseInt(e.target.dataset.roletype)
-    
-    this.checkrole(roletype)
-    
-    (this.data.storeid == '') ? (this.initShelve(roletype)) :
+
+    this.checkrole(roletype);
+
+    (this.data.storeid == '') ? 
+    (this.initShelve(roletype)) : 
     (this.jOrqShelve(roletype, this.data.storeid))
 
   },
   changeIcon(roletype) {
 
-    var iconflag = {}
-    var img = {}
-    var icon = {}
-
-    var iconflagname = "iconlist[" + roletype + "].iconflag"
-    var imgname = "iconlist[" + roletype + "].img"
-    var iconname = "iconlist[" + roletype + "].icon"
+    var iconflag = "iconlist[" + roletype + "].iconflag"
+    var img =      "iconlist[" + roletype + "].img"
+    var icon =     "iconlist[" + roletype + "].icon"
 
     var flag = this.data.iconlist[roletype].iconflag
-    iconflag[iconflagname] = !flag
 
-    console.log(flag)
+    this.setData({
 
-    if (flag) {
-
-      img[imgname] = '../../image/join.png'
-      icon[iconname] = 'roleimg-join'
-    } else {
-
-      img[imgname] = '../../image/quit.png'
-      icon[iconname] = 'roleimg-quit'
-    }
-
-    this.setData(iconflag)
-    this.setData(img)
-    this.setData(icon)
-
-    // console.log(this.data.iconlist)
+      [img]: (!flag) ? '../../image/quit.png' :'../../image/join.png',
+      [icon]: (!flag) ? 'roleimg-quit' : 'roleimg-join',
+      [iconflag]: (!flag)
+    })
 
   },
   roleQuery() {
@@ -259,16 +254,13 @@ Page({
       header: { 'content-type': 'application/x-www-form-urlencoded' }
     }).then(res => {
 
-      // console.log(res)
       if (res.ec == '000000') {
 
-        var itemtmp = {}
-        var itemtmpname = "storeList[" + idx + "]." +
-          ((roletype == 0) ? 'field' : ((roletype == 1) ? 'deal' : 'supply'))
-        // console.log(itemtmpname)
-        itemtmp[itemtmpname] = (optype == 'quit') ? '' : this.data.user
-        this.setData(itemtmp)
-        // console.log(this.data.storeList)
+        var item = "storeList[" + idx + "]." + ((roletype == 0) ? 'field' :
+         ((roletype == 1) ? 'deal' : 'supply'))
+        this.setData({
+          [item]: (optype == 'quit') ? '' : this.data.user
+        })
 
       }
 
